722076c53e0a056011f3793d5f69bf77f9aed4e1
Andres_Berrio <andrewsolutions81@gmail.com>
2022-07-18 18:24:01 -0500

    all runing

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import './App.css';
 import { About } from './components/About';
 import { Home } from './components/Home';
 import { Pricing } from './components/Pricing';
-import {Router, Routes, Link,} from "react-router-dom";
+import {Routes, Route,} from "react-router-dom";
 
 function App() {
   return (
-      <Routes>
-      <Link path="/src/components/Home.jsx" element={<Home />} />
-      <Link path="/src/components/Home.jsx" element={<About />} />
-      <Link path="/src/components/Pricing.jsx" element={<Pricing />} />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path='/about' element={<About/>} />
+      <Route path='/pricing' element={<Pricing/>} />
     </Routes>
   );
 }
